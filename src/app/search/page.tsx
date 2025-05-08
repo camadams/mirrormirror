78e768d0bbef038d823da0e2b3faf5cb56ea9392
cameron.adams <camgadams@gmail.com>
2025-05-08 09:30:31 +0200

    feat: side bar with compare and search view

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Search() {
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20  sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Home</h1>
+      <h1>Search</h1>
     </div>
   );
 }
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,54 @@
+import { Scale, Search } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Compare",
+    url: "/compare",
+    icon: Scale,
+  },
+  {
+    title: "Search",
+    url: "/search",
+    icon: Search,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Mirror Mirror</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span className="font-[family-name:var(--font-geist-sans)]">
+                        {item.title}
+                      </span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
